c3024ab7f5e00172e322c81586f47bdfcd9b82e3
JeonJeSik <wptlr456@naver.com>
2023-04-10 11:19:18 +0900

    Update Study

---
+++ MyStudy/MyStudy/Backjun1012.cpp
@@ -0,0 +1,13 @@
+#include "Backjun1012.h"
+
+Backjun1012::Backjun1012()
+{
+}
+
+Backjun1012::~Backjun1012()
+{
+}
+
+void Backjun1012::Start()
+{
+}
---
+++ MyStudy/MyStudy/Backjun1012.h
@@ -0,0 +1,11 @@
+#pragma once
+class Backjun1012
+{
+public:
+	Backjun1012();
+	~Backjun1012();
+
+public:
+	void Start();
+};
+
